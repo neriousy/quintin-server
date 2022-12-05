4a4bc69eb91269ce294480509eb228fd1f172f18
neriousy <filipori7@interia.pl>
2022-12-05 19:27:00 +0100

    fame route changes

--- routes/Fame.js
+++ routes/Fame.js
@@ -14,8 +14,10 @@ fameRouter.get('/:year/:month', async (req, res)=>{
     const query = `SELECT + ${currentTable}.id, ${currentTable}.fame FROM ${currentTable}`;
     const results = await db.promise().query(query);
     res.status(200).send(results[0]);
+    return; 
   }catch{
     res.status(404).send('Not found');
+    return;
   }
 });
 
@@ -26,29 +28,43 @@ fameRouter.get('/', async (req, res)=>{
 });
 
 fameRouter.get('/progress/:year/:month', async(req, res)=>{
-  const currentMonth = parseInt(req.params.month);
-  const currentYear = parseInt(req.params.year);
-  
+  const date = new Date();
+  let prevTable;
+
   try{
+    const currentMonth = parseInt(req.params.month);
+    const currentYear = parseInt(req.params.year);
+
     let prevMonth = currentMonth - 1;
     let prevYear = currentYear;
   
+    if(currentMonth == date.getMonth() + 1 && currentYear == date.getFullYear()){
+      prevTable = `fame_${currentMonth}_${currentYear}`;
+
+      const query = `SELECT famlist.id, famlist.nick, famlist.class, fame.fame as currentFame, ${prevTable}.fame as prevFame, (fame.fame - ${prevTable}.fame) as diff from famlist INNER JOIN fame on fame.id = famlist.id INNER JOIN ${prevTable} on ${prevTable}.id = famlist.id;`;
+
+      const results = await db.promise().query(query);
+      res.status(200).send(results[0]);
+      return;
+    }
+
     if(currentMonth == 1){
       prevMonth = 12;
       prevYear = currentYear - 1;
     }
 
     const currentTable = `fame_${currentMonth}_${currentYear}`;
-    const prevTable = `fame_${prevMonth}_${prevYear}`;
+    prevTable = `fame_${prevMonth}_${prevYear}`;
 
     const query = `SELECT famlist.id, famlist.nick, famlist.class, ${currentTable}.fame as currentFame, ${prevTable}.fame as prevFame,  (${currentTable}.fame - ${prevTable}.fame) as diff from famlist INNER JOIN ${currentTable} ON ${currentTable}.id = famlist.id INNER JOIN ${prevTable} on ${prevTable}.id = famlist.id`;
     
     const results = await db.promise().query(query);
     res.status(200).send(results[0]);
+    return ;
   }catch{
     res.status(404).send('Not found');
+    return;
   }
-
 });
 
 
